8c7883cfc8d4205244c4aa2a7c4711557cf32d9f
AdamVacha <adamrvacha@gmail.com>
2025-03-23 00:30:21 -0400

    updated hierarchy

--- src/routes/lessons/subject/[subject]/module/[module]/lesson/[lesson]/+page.server.ts
+++
@@ -1,7 +0,0 @@
-// import { db } from "$lib/server/db";
-// import type { PageServerLoad } from "./$types";
-
-// export const load: PageServerLoad = async ({params}) => {
-
-//     const { subjects, modules, lessons} = params;
-// }
