d7b898068e7736206987abd082c5a27167fca690
AdamVacha <adamrvacha@gmail.com>
2025-04-21 17:29:30 -0400

    prod-update10

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from 'drizzle-kit';
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+// src/lib/server/db/index.ts
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
 
-export default defineConfig({
-	schema: './src/lib/server/db/schema.ts',
-	out: './drizzle',
-	dbCredentials: {
-		url: process.env.DATABASE_URL,
-		ssl: true
-	},
-	verbose: true,
-	strict: true,
-	dialect: 'postgresql'
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false
+	}
 });
+
+export const db = drizzle(pool);
