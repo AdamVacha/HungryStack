51a9ada09648f1ff789e80034774f62a32a89c86
AdamVacha <adamrvacha@gmail.com>
2025-04-21 16:19:34 -0400

    prod-update7

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -3,8 +3,13 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from '$lib/server/db/schema';
 
-const dbUrl = env.DATABASE_URL;
+const dbUrl =
+	process.env.DATABASE_URL ||
+	(typeof env !== 'undefined' && env.DATABASE_URL) ||
+	(typeof process.env.DATABASE_URL !== 'undefined' && process.env.DATABASE_URL);
+
 if (!dbUrl) throw new Error('DATABASE_URL is required');
+
 const client = postgres(dbUrl, {
 	ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: true } : false
 });
