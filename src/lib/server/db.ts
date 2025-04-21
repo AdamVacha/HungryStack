95c386422534ebf3ecece4b09ecf7e87285834fc
AdamVacha <adamrvacha@gmail.com>
2025-04-21 18:24:39 -0400

    prod-update13

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,12 +1,8 @@
-import { env } from '$env/dynamic/private';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from '$lib/server/db/schema';
 
-const dbUrl =
-	process.env.DATABASE_URL ||
-	env.DATABASE_URL ||
-	'postgres://postgres:postgres@hungrystack-db.cjcg24aqon9g.us-east-2.rds.amazonaws.com:5432/hungrystackdb';
+const dbUrl = process.env.DATABASE_URL;
 
 if (!dbUrl) throw new Error('DATABASE_URL is required (db-ts)');
 
