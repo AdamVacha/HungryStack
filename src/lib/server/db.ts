d3218c67326e4dae5b7969ba396e6da989e8e961
AdamVacha <adamrvacha@gmail.com>
2025-04-21 16:35:14 -0400

    prod-update-8

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -5,8 +5,8 @@ import * as schema from '$lib/server/db/schema';
 
 const dbUrl =
 	process.env.DATABASE_URL ||
-	(typeof env !== 'undefined' && env.DATABASE_URL) ||
-	(typeof process.env.DATABASE_URL !== 'undefined' && process.env.DATABASE_URL);
+	env.DATABASE_URL ||
+	'postgres://postgres:postgres@hungrystack-db.cjcg24aqon9g.us-east-2.rds.amazonaws.com:5432/hungrystackdb';
 
 if (!dbUrl) throw new Error('DATABASE_URL is required');
 
